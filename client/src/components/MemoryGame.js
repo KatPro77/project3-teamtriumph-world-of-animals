9c496cc42f4d7ddc50e0a716475125c1baa7eccb
jayanti18 <jayantirajesh@yahoo.com>
2018-12-09 20:38:11 -0500

    test

--- client/src/components/MemoryGame.js
+++ client/src/components/MemoryGame.js
@@ -97,7 +97,7 @@ class MemoryGame extends React.Component {
             )
           })}
            <div className='col-md-2'>
-           <a href="ClickeyGame"><input type="button"className="Mybutton" value="Play Again"></input></a>
+           <a href="games"><input type="button"className="Mybutton" value="Play Again"></input></a>
       </div>
         </div>
         </Animated>
